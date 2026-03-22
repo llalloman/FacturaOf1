8bb326ec7e7f23d8552c6b65e24a8e60584ff3c7
llalloman <llallowmol@hotmail.com>
2026-03-21 23:08:37 -0500

    Soft-delete products with traceability

--- apps/productos/urls.py
+++ apps/productos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework import viewsets, filters
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models.deletion import ProtectedError
 from .models import Producto
 from .serializers import ProductoSerializer
 from apps.core.export_mixin import ExportMixin
@@ -37,6 +40,23 @@ class ProductoViewSet(ExportMixin, viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(empresa=self.request.user.empresa)
+
+    def destroy(self, request, *args, **kwargs):
+        producto = self.get_object()
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except ProtectedError:
+            if producto.activo:
+                producto.activo = False
+                producto.save(update_fields=['activo'])
+            return Response(
+                {
+                    'success': True,
+                    'soft_deleted': True,
+                    'mensaje': 'El producto tiene movimientos o documentos asociados. Se marcó como inactivo para conservar la trazabilidad.',
+                },
+                status=status.HTTP_200_OK,
+            )
 
 
 router = DefaultRouter()
